9053b38bc7ad60c55fc30731385956cd96e080ee
Devanshi <81722570+devanshis12@users.noreply.github.com>
2023-12-11 16:01:41 -0500

    Update project.py

--- project.py
+++ project.py
@@ -124,3 +124,70 @@ class CategoricalTruePositives(tf.keras.metrics.Metric):
 
         return self.cat_true_positives
 
+# get images from downloaded kaggle datasets
+mal1_train = load_images(<path to dataset>)
+mal1_test = load_images(<path to dataset>)
+norm1_train = load_images(<path to dataset>)
+norm1_test = load_images(<path to dataset>)
+mal2 = load_images(<path to dataset>)
+norm2 = load_images(<path to dataset>)
+
+# combine images and labels
+norms = [0] * (455+123+416)
+mals = [1] * (561+80+460)
+data = np.stack(norm1_train + norm1_test + norm2 + mal1_train + mal1_test + mal2)
+labels = np.concatenate((norms,mals))
+
+# create tensor dataset
+dataset = tf.data.Dataset.from_tensor_slices((data, labels))
+batched_ds = dataset.batch(32)
+train_ds, val_ds = tf.keras.utils.split_dataset(
+    batched_ds, left_size=0.7, right_size=0.3, shuffle=True, seed=None
+)
+
+# seperate into training and testing datasets
+x_test = np.concatenate([x for x, y in val_ds], axis=0)
+y_test = np.concatenate([y for x, y in val_ds], axis=0)
+x_train = np.concatenate([x for x, y in train_ds], axis=0)
+y_train = np.concatenate([y for x, y in train_ds], axis=0)
+
+# define hyperparameters (l2 norm clip and learning rate from hyperparameter tuning)
+l2_norm_clip = 0.7
+num_microbatches = 1
+learning_rate = 0.2
+epoch = 30
+batch_size = 32
+epochs_per_report = 2
+all_reports = []
+histories = []
+
+# start with zero and increase noise to increase privacy
+noise_multipliers = [0,0.4,0.8,1,1.4,1.8,2]
+
+for noise in noise_multipliers:
+    # ResNet50 model
+    resnet_50 = ResNet50(classes=2, include_top=True, weights=None, input_shape=(224,224,3))
+
+    optimizer = tensorflow_privacy.DPKerasAdamOptimizer(
+        l2_norm_clip=l2_norm_clip,
+        noise_multiplier=noise,
+        num_microbatches=num_microbatches, # take this out
+        learning_rate=learning_rate)
+
+    # compile the resnet model
+    resnet_50.compile(optimizer=optimizer,
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy',
+                       CategoricalTruePositives(2,32)])
+
+    title = ("ResNet50, noise: ", noise)
+    callback = PrivacyMetrics(epochs_per_report, title)
+    history = resnet_50.fit(x_train,
+                        y_train,
+                        validation_data=(x_test,y_test),
+                        epochs=epoch,
+                        batch_size=batch_size,
+                        callbacks=[callback],
+                        shuffle=True)
+    histories.append(history)
+    all_reports.extend(callback.attack_results)
